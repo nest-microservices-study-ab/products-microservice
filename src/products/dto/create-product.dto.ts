78d49189a59ac472afb6a632f9d2722949f8bc95
alejoboga20 <alejoboga19@gmail.com>
2024-12-22 15:44:27 -0300

    create dto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,14 @@
-export class CreateProductDto {}
+import { IsNumber, IsString, Min } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class CreateProductDto {
+  @IsString()
+  name: string;
+
+  @IsNumber({
+    maxDecimalPlaces: 4,
+  })
+  @Min(0)
+  @Type(() => Number)
+  price: number;
+}
