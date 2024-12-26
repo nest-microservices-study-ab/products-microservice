a03416a87ddfb96ab31b2b21534937844fd89503
alejoboga20 <alejoboga19@gmail.com>
2024-12-26 15:12:33 -0300

    clean imports

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,10 +1,4 @@
-import {
-  HttpStatus,
-  Injectable,
-  Logger,
-  NotFoundException,
-  OnModuleInit,
-} from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
